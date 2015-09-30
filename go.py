58db45260c631bcad03918d09c4ffd5437f6b5b4
Kyle Pittman <pittman.kyle@gmail.com>
2015-09-29 19:40:46 -0500

    Lots of changes for python3

--- go.py
+++ go.py
@@ -1,4 +1,6 @@
-from deuces import Card, Evaluator, Deck
+from __future__ import print_function
+
+from .deuces import Card, Evaluator, Deck
 
 # create a card
 card = Card.new('Qh')
@@ -22,22 +24,22 @@ evaluator = Evaluator()
 
 # and rank your hand
 rank = evaluator.evaluate(board, hand)
-print "Rank for your hand is: %d" % rank
+print("Rank for your hand is: {rank}".format(rank=rank))
 
 # or for random cards or games, create a deck
-print "Dealing a new hand..."
+print("Dealing a new hand...")
 deck = Deck()
 board = deck.draw(5)
 player1_hand = deck.draw(2)
 player2_hand = deck.draw(2)
 
-print "The board:"
+print("The board:")
 Card.print_pretty_cards(board)
 
-print "Player 1's cards:"
+print("Player 1's cards:")
 Card.print_pretty_cards(player1_hand)
 
-print "Player 2's cards:"
+print("Player 2's cards:")
 Card.print_pretty_cards(player2_hand)
 
 p1_score = evaluator.evaluate(board, player1_hand)
@@ -48,8 +50,9 @@ p1_class = evaluator.get_rank_class(p1_score)
 p2_class = evaluator.get_rank_class(p2_score)
 
 # or get a human-friendly string to describe the score
-print "Player 1 hand rank = %d (%s)" % (p1_score, evaluator.class_to_string(p1_class))
-print "Player 2 hand rank = %d (%s)" % (p2_score, evaluator.class_to_string(p2_class))
+score_string = "Player {player_number} hand rank = {score} ({evaluation})"
+print(score_string.format(player_number="1", score=p1_score, evalution=evaluator.class_to_string(p1_class))
+print(score_string.format(player_number="2", score=p2_score, evalution=evaluator.class_to_string(p2_class))
 
 # or just a summary of the entire hand
 hands = [player1_hand, player2_hand]
